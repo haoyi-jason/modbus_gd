173c3788b1613edb59ecdd3539e98615ea88d430
jason <jjyeh1976@gmail.com>
2018-02-01 22:32:14 +0800

    fix some master functions

---
+++ functions/mbfuncmaster.c
@@ -0,0 +1,12 @@
+/* ----------------------- System includes ----------------------------------*/
+#include "stdlib.h"
+#include "string.h"
+
+/* ----------------------- Platform includes --------------------------------*/
+#include "port.h"
+
+/* ----------------------- Modbus includes ----------------------------------*/
+#include "mb.h"
+#include "mbproto.h"
+
+
